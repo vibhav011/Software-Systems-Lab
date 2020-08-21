5b1c13b163c27760185adc85303bb755ee2dc924
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-08-21 19:30:53 +0530

    Significantly reduces chars

---
+++ outlab 1/Task1-Integers/roman2.sh
@@ -0,0 +1,21 @@
+n=(100 90 50 40 10 9 5 4 1)
+c=('C' 'XC' 'L' 'XL' 'X' 'IX' 'V' 'IV' 'I')
+
+f(){
+	z=$1
+	for i in {0..8}
+	do
+		if [ $z -ge ${n[$i]} ]
+		then
+			echo "${c[$i]}\c"
+			z=$((z-${n[$i]}))
+			break
+		fi
+	done
+	if [ $z -gt 0 ]
+	then
+		f $z
+	fi
+}
+f $1
+echo

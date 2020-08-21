fad6cad642777a06e067028fc4ebf7fd995512ee
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-08-22 02:15:33 +0530

    Made story.sh recursive

--- outlab 1/Task5-HiddenLore/story.sh
+++ outlab 1/Task5-HiddenLore/story.sh
@@ -1,26 +1,16 @@
 #! /bin/bash
 
 dir=$1
-next=$2
 out=$3
 
-files=($( grep -n "^$next$" $dir/* | grep ':2:' | cut -d: -f1 ))
-
-if [ ${#files[@]} -eq 0 ]
-then
-	echo '' > $out
-else
-	echo $(cat ${files[0]} | head -1) > $out
-	next=$(cat ${files[0]} | tail -1)
-	cur=${files[0]}
-	files=($( grep -n "^$next$" $dir/* | grep ':2:' | cut -d: -f1 ))
-fi
-
-while [ ${#files[@]} -eq 1 ]
-do
-	echo $(cat ${files[0]} | head -1) >> $out
-	next=$(cat ${files[0]} | tail -1)
-	cur=${files[0]}
-	files=($( grep -n "^$next$" $dir/* | grep ':2:' | cut -d: -f1 ))
-done
-
+f() {
+	files=($( grep -n "^$1$" $dir/* | grep ':2:' | cut -d: -f1 ))
+	if [ ${#files[@]} -eq 1 ]
+	then
+		echo $(cat ${files[0]} | head -1) >> $out
+		cur=${files[0]}
+		f $(cat ${files[0]} | tail -1)
+	fi
+}
+echo -n '' > $out
+f $2

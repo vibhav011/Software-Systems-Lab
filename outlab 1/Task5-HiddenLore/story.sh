9a349032b37b282bf1f5f4d3de25646c9e52b2d9
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-08-22 02:04:43 +0530

    Some improvements in story.sh

--- outlab 1/Task5-HiddenLore/story.sh
+++ outlab 1/Task5-HiddenLore/story.sh
@@ -18,17 +18,9 @@ fi
 
 while [ ${#files[@]} -eq 1 ]
 do
-	for i in 0 1
-	do
-		file=${files[$i]}
-		if [[ $file != $cur ]]
-		then
-			echo $(cat $file | head -1) >> $out
-			next=$(cat $file | tail -1)
-			cur=$file
-			break
-		fi
-	done
+	echo $(cat ${files[0]} | head -1) >> $out
+	next=$(cat ${files[0]} | tail -1)
+	cur=${files[0]}
 	files=($( grep -n "^$next$" $dir/* | grep ':2:' | cut -d: -f1 ))
 done
 

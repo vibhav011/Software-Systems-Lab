a9e1165f39bc5e9d578318efc8bda225190a5fd0
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-09-05 20:57:43 +0530

    Removed testing code

--- outlab 3/q6.py
+++ outlab 3/q6.py
@@ -6,6 +6,3 @@ def collapse(L):
 	if len(L) == 1:
 		return collapse(L[0])
 	return functools.reduce(lambda x, y: collapse(x) + ' ' + collapse(y), L)
-
-L = [ ["this","is"], [ ["an", "interesting", "python"], ["programming", "exercise."] ] ]
-print(collapse(L))

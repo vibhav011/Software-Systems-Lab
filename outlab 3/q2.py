fca946ce641e9476fa3b0e06d8706eaac14acfab
akj0811 <jain.ankit201108@gmail.com>
2020-09-05 16:32:45 +0530

    Galti se changes q2

--- outlab 3/q2.py
+++ outlab 3/q2.py
@@ -1,13 +1,13 @@
 import argparse
 import os
 parser = argparse.ArgumentParser()
-parser.add_argument("-in", required = True)
-parser.add_argument("-out", required = True)
-args = vars(parser.parse_args())
+parser.add_argument("-in", "--input", required = True)
+parser.add_argument("-out", "--output", required = True)
+args = parser.parse_args()
 
-with open(args["in"], 'r') as f:
+with open(args.input, 'r') as f:
 	absolute = f.read()
 
-with open(args["out"], 'w') as f:
+with open(args.output, 'w') as f:
 	canon = os.path.realpath(absolute)
 	f.write(canon)

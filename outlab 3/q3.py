c93115913dce5ad60d04afe73e10721013c6b0c7
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-09-06 15:28:58 +0530

    Made code shorter

--- outlab 3/q3.py
+++ outlab 3/q3.py
@@ -1,5 +1,5 @@
 import argparse
-from collections import Counter
+from collections import Counter, defaultdict
 
 my_parser = argparse.ArgumentParser()
 my_parser.add_argument('-ca', type=str, required=True)
@@ -19,24 +19,16 @@ for cnd in types:
 
 N = int(child[0])
 
-demand = {}						# dict which stores candy type vs list of money offerred
+demand = defaultdict(list)				# dict which stores candy type vs list of money offerred
 for i in range(1, N+1):
 	t, m = map(int, child[i].split())
-	if t in demand.keys():
-		demand[t].append(m)
-	else:
-		demand[t] = [m]
+	demand[t].append(m)
 
 ans = 0
 for tp in demand:
-	if tp not in freq.keys():
-		continue
 	demand[tp].sort(reverse = True)
-	temp = 0
 	for i in range(min(freq[tp], len(demand[tp]))):
-		temp += demand[tp][i]
-	ans += temp
+		ans += demand[tp][i]
 
 print(ans)
-
 

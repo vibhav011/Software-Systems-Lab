59c7b2d96a2b9c701f3cbb2ab5f67d00d49da5f7
akj0811 <jain.ankit201108@gmail.com>
2020-09-11 12:16:31 +0530

    Added Task5

---
+++ outlab 4/task5/task5.py
@@ -0,0 +1,33 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+init = (requests.get('https://api.covid19india.org/csv/latest/case_time_series.csv').text).split('\n')
+data = [init[i].split(',') for i in range(len(init))]
+
+for i in range(len(data)):
+	if data[i][0].strip() == "15 April":
+		loc = i
+		break
+
+y = np.array([float(data[i][6])/float(data[i-1][6]) for i in range(loc, len(data))])
+x = np.arange(1, len(data)-loc+1)
+
+plt.plot(x, y, 'r', label="original data")
+
+slope, intercept, a, b, c = stats.linregress(x, y)
+ans = int(np.ceil((1-intercept)/slope))
+
+xx = np.linspace(1, ans+10)
+yy = xx*slope+intercept
+
+plt.plot(xx, yy, 'b', label="regressed data")
+plt.xlabel('t')
+plt.ylabel('H(t)')
+plt.title("covid19india")
+plt.legend()
+plt.savefig("covid.png")
+plt.close()
+
+print(ans)

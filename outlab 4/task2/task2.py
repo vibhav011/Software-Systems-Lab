a28bbb45b927f7d8514ec61acde110b8837981a6
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-09-12 23:47:14 +0530

    Added task 2

--- outlab 4/task2/task2.py
+++ outlab 4/task2/task2.py
@@ -8,21 +8,52 @@ parser.add_argument('--data', type=str, required=True)
 args = parser.parse_args()
 
 data = pd.read_csv(args.data)
-print(data.head())
 
-# df = pd.DataFrame({
-#     'name':['john','mary','peter','jeff','bill','lisa','jose'],
-#     'age':[23,78,22,19,45,33,20],
-#     'gender':['M','F','M','M','M','F','M'],
-#     'state':['california','dc','california','dc','california','texas','texas'],
-#     'num_children':[2,0,0,3,2,1,4],
-#     'num_pets':[5,1,0,5,2,2,3]
-# })
+instances = ['i-1.txt', 'i-2.txt', 'i-3.txt']
+filenames = ['instance1.png', 'instance2.png', 'instance3.png']
+algos = ['round-robin', 'ucb', 'kl-ucb', 'thompson-sampling']
+ep_values = [0.2, 0.02, 0.002]
 
-for ind in data.index:
-	print(data['REG'][ind])
-	if ind == 10:
-		break
+grouped = data.groupby(['instance'])
 
-# df.plot(kind='scatter',x='num_children',y='num_pets',color='red')
-# plt.savefig('instance1.png')
+for (ins, fname) in zip(instances, filenames):
+	df = grouped.get_group(ins)
+	grped = df.groupby(['algorithm'])
+	
+	plt.figure()
+	plt.xlabel('horizon')
+	plt.ylabel('Regret')
+	plt.title('Instance ' + ins[2] + ' - both axes in log scale')
+
+	for algo in algos :
+		grpByAlgo = grped.get_group(algo)
+		uniqueHorizons = list(grpByAlgo['horizon'].unique())
+		uniqueHorizons.sort()
+		grpByHor = grpByAlgo.groupby(['horizon'])
+
+		reg_mean = []
+
+		for hor in uniqueHorizons :
+			samples = grpByHor.get_group(hor).sample(n = 50)
+			reg_mean.append(samples['REG'].mean())
+
+		plt.loglog(uniqueHorizons, reg_mean)
+	
+	ep_groups = grped.get_group('epsilon-greedy').groupby('epsilon')
+
+	for ep in ep_values :
+		grpByEp = ep_groups.get_group(ep)
+		uniqueHorizons = list(grpByEp['horizon'].unique())
+		uniqueHorizons.sort()
+		grpByHor = grpByEp.groupby(['horizon'])
+
+		reg_mean = []
+
+		for hor in uniqueHorizons :
+			samples = grpByHor.get_group(hor).sample(n = 50)
+			reg_mean.append(samples['REG'].mean())
+
+		plt.loglog(uniqueHorizons, reg_mean)
+	
+	plt.legend(algos + ['epsilon-greedy with epsilon=' + str(ep) for ep in ep_values])
+	plt.savefig(fname)

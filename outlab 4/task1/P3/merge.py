fc482697f6c1127eb6306f5a539b8fda2fee5510
akj0811 <jain.ankit201108@gmail.com>
2020-09-13 17:20:13 +0530

    Task1 Updated

--- outlab 4/task1/P3/merge.py
+++ outlab 4/task1/P3/merge.py
@@ -1,34 +1,26 @@
 import csv
 import numpy as np
 
-data_lock = np.zeros((7, 2))
-data_unlock = np.zeros((7, 2))
-
 days = []				# Mon, Tues, etc.
 
-with open('mumbai_data.csv') as csvfile:
+with open('./mumbai_data.csv') as csvfile:
 	file_object = csv.reader(csvfile)
 	file_object.__iter__().__next__()		# skips the header line
-	i = 0
 	for row in file_object:
 		days.append(row[0])
-		for j in [0, 1] :
-			data_lock[i, j] = row[j+1]
-		i += 1
 
-with open('mumbai_unlock.csv') as csvfile:
-	file_object = csv.reader(csvfile)
-	file_object.__iter__().__next__()		# skips the header line
-	i = 0
-	for row in file_object:
-		for j in [0, 1] :
-			data_unlock[i, j] = row[j+1]
-		i += 1
+with open('./mumbai_data.csv') as csvfile:
+	data_lock = np.genfromtxt(csvfile, delimiter = ',')
+	data_lock = data_lock[1:, 1:3]
+
+with open('./mumbai_unlock.csv') as csvfile:
+	data_unlock = np.genfromtxt(csvfile, delimiter = ',')
+	data_unlock = data_unlock[1:, 1:3]
 
 data_lock[:, 0] = data_lock[:, 1]/data_lock[:, 0]
 data_unlock[:, 0] = data_unlock[:, 1]/data_unlock[:, 0]
 
-with open('info_combine.csv', 'w+') as csvfile:
+with open('./info_combine.csv', 'w+') as csvfile:
 	file_object = csv.writer(csvfile)
 	file_object.writerow(['Day', 'Infected(UnLock)', 'Infected(Lock)', 'Positivity Rate(Lock)', 'Positivity Rate(UnLock)'])
 
@@ -39,6 +31,3 @@ with open('info_combine.csv', 'w+') as csvfile:
 							  round(data_lock[i, 0], 3),
 							  round(data_unlock[i, 0], 3),
 							])
-
-
-

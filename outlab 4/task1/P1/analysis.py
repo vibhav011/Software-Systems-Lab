7783de042f31fd939105a4bde68dacc2894f8a2c
akj0811 <jain.ankit201108@gmail.com>
2020-09-13 17:01:39 +0530

    Unnecessary changes git add .

--- outlab 4/task1/P1/analysis.py
+++ outlab 4/task1/P1/analysis.py
@@ -1,24 +1,17 @@
-import csv
 import numpy as np
 
-data = np.zeros((7, 4))
-
 with open('./mumbai_data.csv') as csvfile:
-	file_object = csv.reader(csvfile)
-	file_object.__iter__().__next__()		# skips the header line
-	i = 0
-	for row in file_object:
-		for j in [0, 1, 2, 3] :
-			data[i, j] = row[j+1]
-		i += 1
+	data = np.genfromtxt(csvfile, delimiter=',')
+	data = data[1:, 1:]
 
 f = lambda t : format(round(t, 3), '.3f')		# rounding function
 mean = list(map(f, data.mean(axis=0)))
 std = list(map(f, data.std(axis=0)))
 
 cols = ['Tests', 'Infected', 'Recovered', 'Deceased']
+heads = ['Field', 'Mean', 'Std. Dev.']
 
-print ("{:<9} {:>13} {:>12}".format('Field', 'Mean', 'Std. Dev.'))
+final = [heads] + list(zip(cols, mean, std))	# Merges all the data
 
-for i in range(4) :
-	print ("{:<9} {:>13} {:>12}".format(cols[i], mean[i], std[i]))
+for i in range(5):
+	print ("{:<9}\t{:>10}\t{:>11}".format(*final[i]))

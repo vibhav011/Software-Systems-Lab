0c91545a090cb85ef2419ea9cda061f2c0f0539b
akj0811 <jain.ankit201108@gmail.com>
2020-09-13 17:00:45 +0530

    Unnecessary changesgit add .

--- outlab 4/task1/P2/convert.py
+++ outlab 4/task1/P2/convert.py
@@ -1,34 +1,29 @@
 import csv
 import numpy as np
 
-data = np.zeros((7, 4))
-
 days = []				# Mon, Tues, etc.
 
-with open('mumbai_data.csv') as csvfile:
+with open('./mumbai_data.csv') as csvfile:
 	file_object = csv.reader(csvfile)
 	file_object.__iter__().__next__()		# skips the header line
-	i = 0
 	for row in file_object:
 		days.append(row[0])
-		for j in [0, 1, 2, 3] :
-			data[i, j] = row[j+1]
-		i += 1
+
+with open('./mumbai_data.csv') as csvfile:
+	data = np.genfromtxt(csvfile, delimiter = ',')
+	data = data[1:, 1:]
 
 data[:, 1] /= data[:, 0]		# test positivity rate
 data[:, 0] /= 20.4				# tests per million
 
-with open('transformed.csv', 'w+') as csvfile:
+with open('./transformed.csv', 'w+') as csvfile:
 	file_object = csv.writer(csvfile)
-	file_object.writerow(['Day', 'Tests', 'Infected', 'Recovered', 'Deceased'])
+	file_object.writerow(['Day', 'Tests per Million', 'Positivity rate', 'Recovered', 'Deceased'])
 
-	for i in range(7):
+	for i in range(len(days)):
 		file_object.writerow([days[i],
 							  int(round(data[i, 0])),
 							  round(data[i, 1], 3),
 							  int(data[i, 2]),
 							  int(data[i, 3])
 							])
-
-
-

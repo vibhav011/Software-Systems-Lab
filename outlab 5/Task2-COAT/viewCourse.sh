3daefe5a8ab9d67faf576194e83af8790f7637ad
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-09-28 22:35:35 +0530

    Fixed a small bug

--- outlab 5/Task2-COAT/viewCourse.sh
+++ outlab 5/Task2-COAT/viewCourse.sh
@@ -2,7 +2,7 @@
 
 pattern=$2
 awk 'NR<=3 {print}' $1
-awk -v var="$pattern" '$0~var{
+awk -v var="$pattern" '($0~var)&&(NR>3){
     num = 3;
     if ($2~/^[0-9]/)
         num = 4;
--- outlab 5/Task2-COAT/viewSemester.sh
+++ outlab 5/Task2-COAT/viewSemester.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 awk 'NR<=3 {print}' $1
-awk -v sem=$2 -v year=$3 '($0~sem)&&($0~year){
+awk -v sem=$2 -v year=$3 '($0~sem)&&($0~year)&&(NR>3){
 	num = 3;
     if ($2~/^[0-9]/)
         num = 4;

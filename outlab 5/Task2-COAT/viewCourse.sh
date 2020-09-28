fba007e10e112a218a5aa44b0a0a4725677263b4
akj0811 <jain.ankit201108@gmail.com>
2020-09-28 21:22:00 +0530

    Updated Q2 d

--- outlab 5/Task2-COAT/viewCourse.sh
+++ outlab 5/Task2-COAT/viewCourse.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 pattern=$2
-
-awk -v var="$pattern" '
-$0~var{print $0}
+awk -v var="$pattern" '$0~var{
+    num = 3;
+    if ($2~/^[0-9]/)
+        num = 4;
+    print $0 | "sort -k "num}
 ' $1

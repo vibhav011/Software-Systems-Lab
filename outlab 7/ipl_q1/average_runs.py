4f91b7fd3533f88dfec76d482f9079e62b4fae42
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-24 18:24:48 +0530

    Made average_runs.py shorter

--- outlab 7/ipl_q1/average_runs.py
+++ outlab 7/ipl_q1/average_runs.py
@@ -1,27 +1,16 @@
 import sqlite3
-from collections import defaultdict, Counter
 
 conn = sqlite3.connect('ipl.db')
 c = conn.cursor()
 
-matches_runs = c.execute('''SELECT MATCH.venue_name, SUM(BALL_BY_BALL.runs_scored), SUM(BALL_BY_BALL.extra_runs)
-						   FROM MATCH
-						   INNER JOIN BALL_BY_BALL ON MATCH.match_id=BALL_BY_BALL.match_id
-						   GROUP BY MATCH.match_id''')
+matches_runs = c.execute('''SELECT MATCH.venue_name, SUM(BALL_BY_BALL.runs_scored+BALL_BY_BALL.extra_runs), COUNT(DISTINCT MATCH.match_id)
+							FROM MATCH
+							INNER JOIN BALL_BY_BALL ON MATCH.match_id=BALL_BY_BALL.match_id
+							GROUP BY MATCH.venue_name''')
 
-avg_runs = defaultdict(float)
-cnt = Counter()
-
-for match in matches_runs:
-	avg_runs[match[0]] += match[1]
-	cnt[match[0]] += 1
-
-for k in avg_runs:
-	avg_runs[k] /= cnt[k]
-
-sorted_list = [(k, v) for k, v in sorted(avg_runs.items(), key=lambda item: item[1], reverse=True)]
-
-for pair in sorted_list:
-	print("%s,%f" % (pair[0], pair[1]))
+final = [[x, y*1.0/z] for x, y, z in matches_runs]
+final = sorted(final, key = lambda x : (-x[1], x[0]))
+for stadium in final:
+	print("{},{}".format(*stadium))
 
 conn.close()

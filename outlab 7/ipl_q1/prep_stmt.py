211b6ac1568f0f9bc813a149bd77d26a19324db1
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-25 23:23:34 +0530

    Forced foreign_key constraint

--- outlab 7/ipl_q1/prep_stmt.py
+++ outlab 7/ipl_q1/prep_stmt.py
@@ -5,6 +5,7 @@ table_names = ['TEAM', 'PLAYER', 'MATCH', 'PLAYER_MATCH', 'BALL_BY_BALL']
 conn = sqlite3.connect('ipl.db')
 with conn:
 	c = conn.cursor()
+	c.execute("PRAGMA foreign_keys = ON")
 
 	query = "INSERT INTO " + table_names[int(sys.argv[1])-1] + " VALUES (?" + (", ?"*(len(sys.argv)-3)) + ")";
 	vals = tuple(sys.argv[2:])

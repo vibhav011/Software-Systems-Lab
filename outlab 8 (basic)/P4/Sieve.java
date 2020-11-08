55ae6b65a2ba62b0bc26c8b199c4790a4514c68f
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-11-08 21:50:07 +0530

    Added Sieve

--- outlab 8 (basic)/P4/Sieve.java
+++ outlab 8 (basic)/P4/Sieve.java
@@ -1,23 +1,18 @@
 import java.util.*;
 import java.util.stream.*;
+import java.lang.Math;
 
-public class Sieve {
-
-	public static IntStream cnt, sieve;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		cnt = IntStream.range(2,n+1);
-		sieve = IntStream.range(2,n+1);			// non zero means prime
-		
-		cnt.map(x -> {
-			sieve = sieve.map(y -> y%x==0&&x!=y ? 0 : y);
-			return x;
-		}).boxed().collect(Collectors.toList());
-		
-		sieve.filter(x -> x > 0).forEach(x -> System.out.print(x + " "));
-		System.out.println();
-	}
+public class sieve 
+{
+    public static int n = 500000;
+    public static void main(String[] args) 
+    {
+        Set<Integer> initialSet = IntStream.range(2, n).boxed().collect(Collectors.toSet());
+        Set<Integer> primes = IntStream.range(2, (int)Math.sqrt(n) + 1).
+                                mapToObj(x -> IntStream.range(x, n/x).map(a -> a*x).boxed().collect(Collectors.toSet())).
+                                reduce(initialSet, (r, x) -> {SortedSet<Integer> y = new TreeSet<Integer> (r); y.removeAll(x); return y;});
+        System.out.println(primes.size());
+        primes.stream().forEach(x -> System.out.print(x+" "));
+        System.out.println();
+    }
 }

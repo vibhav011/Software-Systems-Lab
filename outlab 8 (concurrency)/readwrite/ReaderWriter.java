f0c739d71ee12969be73665245443e23efb4b0e1
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-11-11 19:26:19 +0530

    Added ReaderWriter

---
+++ outlab 8 (concurrency)/readwrite/ReaderWriter.java
@@ -0,0 +1,31 @@
+package readwrite;
+
+import java.util.Scanner;
+import java.io.File;
+
+class ReaderWriter implements Runnable {
+	private String FILENAME;
+	private ProtectedTree ptree;
+	private boolean iswriter;
+
+	public ReaderWriter(String FILENAME, ProtectedTree ptree, boolean iswriter) {
+		this.FILENAME = FILENAME;
+		this.ptree = ptree;
+		this.iswriter = iswriter;
+	}
+
+	public void run() {
+		File file = new File(this.FILENAME);
+		try {
+			Scanner sc = new Scanner(file);
+
+			while(sc.hasNextLine()) {
+				int val = sc.nextInt();
+
+				if (iswriter) this.ptree.write(val);
+				else this.ptree.read(val);
+			}
+		}
+		catch (Exception e) {}
+	}
+}
